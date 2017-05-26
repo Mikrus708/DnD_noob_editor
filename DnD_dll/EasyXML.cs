9542e6b7e5e3131db01c967a1d32ab43e09d4f7b
Stanisław Piotrowski <stasiek7776@gmail.com>
2017-05-26 16:06:42 +0200

    Easy serializer

---
+++ DnD_dll/EasyXML.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DnD
+{
+    public static class EasyXML
+    {
+        public static bool SerializeXML<T>(T obj, string FilePath)
+        {
+            bool result = true;
+            XmlSerializer ser = new XmlSerializer(typeof(T));
+            FileStream fs = File.Open(FilePath, FileMode.Create);
+            XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+            ns.Add("", "");
+            try
+            {
+                ser.Serialize(fs, obj, ns);
+            }
+            catch
+            {
+                result = false;
+            }
+            fs.Close();
+            return result;
+        }
+        public static T DeserializeXML<T>(string FilePath)
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(T));
+            FileStream fs = File.Open(FilePath, FileMode.Open);
+            T result = default(T);
+            try
+            {
+                result = (T)ser.Deserialize(fs);
+            }
+            catch { }
+            fs.Close();
+            return result;
+        }
+    }
+}
